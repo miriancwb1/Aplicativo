523ecfac019cac94c2e621745cbe25e7b8393651
miriancwb1 <163296336+miriancwb1@users.noreply.github.com>
2024-04-11 08:40:55 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -16,26 +16,51 @@ def voltar_ao_menu_principal():
    input ('\n digite a tecla "enter" para voltar ao menu principal')
    main()
 
+def opcao_invalida():
+ print ('opção invalida!\n')
+ voltar_ao_menu_principal()
 
-opcao_escolhida = int (input ('escolha uma opção'))
-#print ('vocẽ escolheu a opção', opção escolhida)
-#print(f' vocẽ escolheu a opção { opção_ecolhida}')
+
+def listar_restaurante():
+  exibir_subtitulos ('listando os restaurantes')
+
+  for restaurantes in restaurantes:
+    print (f'*{restaurantes}')
+
 
 def exibir_subtitulos (texto):
   os.system ('clear')
   print (texto)
   print ()
 
-if opcao_escolhida == 1: 
- def escolher_opcoes():
-  print ('cadastrar restaurante')
-elif opcao_escolhida == 2:
- print (' listar restaurantes')
-elif opcao_escolhida == 3:
- print ('ativar restaurante')
-else:
-   finalizada_app()
-  
+
+def cadastar_novo_restaurante():
+    exibir_subtitulos ('cadastros de novos restaurante:')
+    nome_do_restaurante = input ('digite o nome do novo restaurante:')
+    restaurantes.append (nome_do_restaurante)
+    print (f'o restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
+    voltar_ao_menu_principal()
+
+
+
+def escolher_opcoes():
+ try:
+
+  opcao_escolhida = int (input ('escolha uma opção'))
+
+  if opcao_escolhida == 1:   
+   cadastar_novo_restaurante
+  elif opcao_escolhida == 2:
+   listar_restaurante()
+  elif opcao_escolhida == 3:
+   print ('ativar restaurante')
+  elif opcao_escolhida == 4:
+    finalizada_app
+  else:
+    opcao_escolhida
+ except:
+   opcao_invalida()
+    
 def main(): 
   os.system('cls')
   exibir_nome_do_progama()
