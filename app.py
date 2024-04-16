5865b9cdd00d77e7371ce0bceb782eefebdb8b98
miriancwb1 <163296336+miriancwb1@users.noreply.github.com>
2024-04-16 10:12:01 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import os
-restaurantes = {'pizza,xp'} #lista de restaurantes
+restaurantes = [{'nome': 'pizza xp','caategoria' : 'alimento', 'ativo' : False},
+                {'nome' : 'santa', 'categoria': 'alimento', 'ativo' : False},
+                {'nome' : 'CWB', 'categoria': 'pizza ', 'ativo': False }]
+        
+
 
 def exibir_nome_do_progama ():
  print ("""sabor expreess""")
@@ -10,6 +14,7 @@ def exibir_opcaoes():
  print ('4 sair')
 
 def finalizada_app (): 
+
  exibir_subtitulos ('finalizar app') 
 
 def voltar_ao_menu_principal():
@@ -30,17 +35,37 @@ def listar_restaurante():
 
 def exibir_subtitulos (texto):
   os.system ('clear')
+  linha = '*' * (len(texto))
+  print (linha)
   print (texto)
+  print (linha)
   print ()
 
 
 def cadastar_novo_restaurante():
     exibir_subtitulos ('cadastros de novos restaurante:')
-    nome_do_restaurante = input ('digite o nome do novo restaurante:')
-    restaurantes.append (nome_do_restaurante)
-    print (f'o restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
-    voltar_ao_menu_principal()
+    nome_do_restaurante = input ('digite o nome do novo restaurante')
+    categoria = input (f'digite a categoria do restaurante' {nome_do_restaurante}:)
+    dado_do_restaurante = {' nome': nome_do_restaurante, 'categoria' : categoria, 'ativo' : False}
+    restaurantes.append (dado_do_restaurante)
+    print (f'o restaurante  {nome_do_restaurante} foi cadastrado com sucesso!')
+    voltar_ao_menu_principal
+    
 
+
+def listar_restaurante ():
+  exibir_subtitulos ('litando os restaurantes')
+
+
+
+print (f'{"nome do restaurante"}.just (22)] | status)')
+      for restaurantes in restaurantes:
+      nome_do_restaurante = restaurantes ['categoria']
+      ativo = 'ativado' if 
+
+
+
+#falta terminar o lista_restaurante
 
 
 def escolher_opcoes():
